ef27c4a44426dddfc01467b80733819d6c701a2b
Wypa712 <morgrim345@gmail.com>
2024-12-15 22:45:35 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Soc_project/', // замените на имя вашего репозитория
+  base: '/Soc_project', // замените на имя вашего репозитория
   plugins: [react()]
 });
  
